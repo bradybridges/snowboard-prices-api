dcc2fdca76019d7fb69da961e6a1300a91e6974d
Brady Bridges <bradyjbridges@gmail.com>
2019-11-28 20:10:01 -0700

    Add GET endpoint for all brands

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const environment = process.env.NOVE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Snowboard Price API';
+
+app.get('/', (request, response) => {
+  response.status(200).send('Welcome to the Snowboard Price API');
+})
+
+app.get('/api/v1/brands', (request, response) => {
+  database('brands')
+    .orderBy('name')
+    .then((data) => {
+      response.status(200).json(data);
+    })
+    .catch((err) => {
+      response.status(500).json({ err });
+    });
+})
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});   
