53f9e1ab9328c344012b9b6ee9fa1ef0851786bd
Brady Bridges <bradyjbridges@gmail.com>
2019-11-28 20:16:19 -0700

    Add GET endpoint for brand by name

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.locals.title = 'Snowboard Price API';
 
 app.get('/', (request, response) => {
   response.status(200).send('Welcome to the Snowboard Price API');
-})
+});
 
 app.get('/api/v1/brands', (request, response) => {
   database('brands')
@@ -25,7 +25,20 @@ app.get('/api/v1/brands', (request, response) => {
     .catch((err) => {
       response.status(500).json({ err });
     });
-})
+});
+
+app.get('/api/v1/brands/:name', (request, response) => {
+  database('brands')
+    .where('name', request.params.name)
+    .then((brand) => {
+      if(brand.length) {
+        return response.status(200).json(brand[0]);
+      } else {
+        return response.status(404).json({ error: `No brand by name ${request.params.name} found` });
+      }
+    })
+    .catch((err) => response.status(500).json({ err }));
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
